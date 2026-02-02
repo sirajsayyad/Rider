d034fc2b580af481a4236a9e1008c56cdf918f6b
corerspp-ship-it <corerspp@gmail.com>
2026-02-02 22:33:38 +0530

    Deploy Flutter Web with vercel.json

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "48ca258fee4d8c181a4c640c6aca888c
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "85e58b14520c1413b9d3f2cc1a3f63dd",
+"flutter_bootstrap.js": "7b0ca14a36bb38b811a0253e1831ec0a",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
