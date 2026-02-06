b72ec0a8cb50011e592a35b4a75b28882f1c0675
Siraj Sayyad <sirajrsayyadd@gmail.com>
2026-02-06 22:48:56 +0530

    Fix base href for Vercel

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "48ca258fee4d8c181a4c640c6aca888c
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "53a7391c11ca72c45841193fc0fd9924",
+"flutter_bootstrap.js": "46d9532245ccc28e1918d4cbf1bef3de",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
